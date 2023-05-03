3b2a22b4c0f76fa438fa57ad7ea40d1c4cefca51
Marcos Lilljedahl <marcosnils@gmail.com>
2023-05-03 15:33:14 -0300

    add workflow to push docker image
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- build.js
+++ build.js
@@ -11,13 +11,28 @@ connect(async (client) => {
   // get reference to the local project
   const source = client.host().directory(".", { exclude: ["node_modules/"] })
 
-  await client.container().from("node:18-alpine")
-    .withServiceBinding("db", database) // bind database with the name db
-    .withEnvVariable("DB_CONNECTION_STRING", "postgres://postgres:test@db:5432/example_db") // db refers to the service binding
+  const base = client.container().from("node:18-alpine")
     .withDirectory("/src", source)
     .withWorkdir("/src")
-    .withExec(["npm", "install"])
-    .withExec(["npm", "run", "test"]) // execute npm run test
-    .stdout()
 
-}, { LogOutput: process.stdout })
+  const test = base
+    .withServiceBinding("db", database) // bind database with the name db
+    .withEnvVariable("DB_CONNECTION_STRING", "postgres://postgres:test@db:5432/example_db") // db refers to the service binding
+    .withExec(["npm", "ci"])
+    .withExec(["npm", "run", "test"]) // execute npm run test
+
+  console.log(await test.stdout())
+  console.log(await test.stderr())
+
+  //push the resulting image
+  console.log("Tests succesfull, pushing docker image:")
+  const imgRef = await base
+    .withExec(["npm", "ci", "--production"])
+    .withEntrypoint(["node", "server.js"])
+    .publish("ttl.sh/tech4impact-dagger:1h")
+
+  console.log(`image pused. ref: ${imgRef}`)
+
+},
+  //{ LogOutput: process.stdout }
+)
